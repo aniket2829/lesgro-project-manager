888c08b50c7eae033a47e65743b13db61ec267c6
Aniket Dhiman <anidhiman456@gmail.com>
2023-11-18 22:47:18 +0530

    Login Page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,29 @@
 // Login Page here
+import Background from "../../assets/Background.png"
 
 export default function Login() {
     return (
-        <h1>Login page</h1>
+        <>
+        <div className="container">
+            <div className="left-bg" style={{backgroundImage:`url(${Background})`}}>
+                
+            </div>
+            <div className="right-bg">
+                <div className="login-form">
+                    <h2>Login</h2>
+                <form action="">
+                    <div className="p:30px">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" placeholder="Enter your Username" name="username" required/>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" placeholder="Enter your Password" name="password" required />
+                        <button>Submit</button>
+                        
+                    </div>
+                </form>
+                </div>
+            </div>
+        </div>
+        </>
     )
 }
