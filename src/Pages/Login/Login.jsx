efe7f87a1418e2dd5f97ecd8ffff732763930976
Aniket Dhiman <anidhiman456@gmail.com>
2023-11-19 15:41:47 +0530

    Login Tailwind CSS applied

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,15 +4,15 @@ import Background from "../../assets/Background.png"
 export default function Login() {
     return (
         <>
-        <div className="container">
-            <div className="left-bg" style={{backgroundImage:`url(${Background})`}}>
+        <div className="flex bg-blue-600 h-screen">
+            <div className="flex-1" style={{backgroundImage:`url(${Background})`}}>
                 
             </div>
-            <div className="right-bg">
-                <div className="login-form">
-                    <h2>Login</h2>
-                <form action="">
-                    <div className="p:30px">
+            <div className="flex flex-1 items-center justify-center">
+                <div className="max-w-md w-full p-10 bg-white rounded-xl shadow-md">
+                    <h2 className="text-center text-xl font-bold">Login</h2>
+                <form className="w-full" action="">
+                    <div className="login-form">
                         <label htmlFor="username">Username</label>
                         <input type="text" placeholder="Enter your Username" name="username" required/>
                         <label htmlFor="password">Password</label>
